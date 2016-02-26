f7422e67b2d6a154938a15477dde183c09cd194a
Jiapei Liang <liang98@purdue.edu>
2016-02-26 10:57:35 -0500

    Re-structure the file structure

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+
+module.exports = {
+	url: 'mongodb://admin:abc123@ds017258.mlab.com:17258/jiapei-todo-app'
+};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 
-// set up ===================================
+// set up ======================================================
 
 var express = require('express');  
 var app = express();  // create our app with express
@@ -8,11 +8,11 @@ var mongoose = require('mongoose');  // mongoose for mongodb
 var morgan = require('morgan');  // log requests to the console (express4)
 var bodyParser = require('body-parser');  // pull information from HTML POST (express4)
 var methodOverride = require('method-override');  // simulate DELETE and PUT (express4)
+var database = require('./config/database');  // load the config
 
+// configuration ===============================================
 
-// configuration ==================================
-
-mongoose.connect('mongodb://admin:abc123@ds017258.mlab.com:17258/jiapei-todo-app');  // connect to mongoDB database on mongolab.com
+mongoose.connect(database.url); // connect to mongoDB database on mongoLab.com
 
 app.use(express.static(__dirname + '/public'));  // set the static files location /public, /public/img will be /img for users
 app.use(morgan('dev'));  // log every request to the console
@@ -21,65 +21,8 @@ app.use(bodyParser.json());  // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-
-// define model ==============================================
-
-var Todo = mongoose.model('Todo', {
-	text : String
-});
-
-
-// routes ====================================================
-
-	// api ---------------------------------------
-
-	//get all todos
-	app.get('/api/todos', function(req, res) {
-		// use mongoose to get all todos in the database
-		Todo.find(function (err, todos) {
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err);
-
-			res.json(todos); // return all todos in JSON format
-		});
-	});
-
-	// create todo and send back all todos after creation
-	app.post('/api/todos/', function (req, res) {
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text: req.body.text,
-			done: false
-		}, function (err, todo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function (err, todos) {
-				if (err)
-					res.send(err);
-				res.json(todos);
-			});
-		});
-	});
-
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function (req, res) {
-		Todo.remove({
-			_id: req.params.todo_id
-		}, function (err, rodo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function (err, todos) {
-				if (err)
-					res.send(err);
-				res.json(todos);
-			});
-		});
-	});
+// routes =======================================================
+require('./app/routes')(app);
 
 
 // listen (start app with node server.js) ========================
@@ -88,7 +31,3 @@ app.listen(3000);
 console.log("App listening on port 3000");
 
 
-// application --------------------------------------------
-app.get('*', function (req, res) {
-	res.sendfile('./public/index.html');  // load the single view file (angular will handle the page changes on the front-end)
-});
