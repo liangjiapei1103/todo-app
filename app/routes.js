2781073309e71458564d35309070608242d165a5
Jiapei Liang <liang98@purdue.edu>
2016-02-27 21:59:28 -0500

    fix auth/facebook

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,7 @@ module.exports = function (app, passport) {
 
 	//process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect: '/profile', // redirect to the secure profile section
+		successRedirect: '/', // redirect to the secure profile section
 		failureRedirect: '/login', // redirect back to the signup page if there is an error
 		failureFlash: true // allow flash messages
 	}));
@@ -32,17 +32,10 @@ module.exports = function (app, passport) {
 
 	// process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/profile', // redirect to the secure profile section
+		successRedirect: '/', // redirect to the secure profile section
 		failureRedirect: '/signup', // redirect back to the signup page if there is an error
 		failureFlash: true // allow flash messages
  	}));
-	
-	// profile page =========================================
-	app.get('/profile', isLoggedIn, function (req, res) {
-		res.render('profile.ejs', {
-			user: req.user // get the user out of session and pass to template
-		});
-	});
 
 
 	// Facebook routes ======================================
@@ -51,7 +44,7 @@ module.exports = function (app, passport) {
 
 	// handle the callback after facebook has authenticated the user
 	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-		successRedirect: '/profile',
+		successRedirect: '/',
 		failureRedirect: '/'
 	}));
 
@@ -62,7 +55,7 @@ module.exports = function (app, passport) {
     // handle the callback after twitter has authenticated the user
     app.get('/auth/twitter/callback',
         passport.authenticate('twitter', {
-            successRedirect : '/profile',
+            successRedirect : '/',
             failureRedirect : '/'
         })
     );
@@ -73,7 +66,7 @@ module.exports = function (app, passport) {
     // the callback after google has authenticated the user
     app.get('/auth/google/callback',
         passport.authenticate('google', {
-            successRedirect : '/profile',
+            successRedirect : '/',
             failureRedirect : '/'
         })
     );
